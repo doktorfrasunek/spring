f0f797390f6745b9b162bcf52fb3d25684f9e318
jedrzej nowak <jendrju@gmail.com>
2019-11-18 07:54:46 +0100

    mark/unmark

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,14 +2,46 @@ $(function(){
    $(".button-collapse").sideNav({edge: 'right'});
 });
 
-$('.mark.favorite').click(function(event){
+function mark(event){
     event.preventDefault();
     var form = event.target;
-     alert(form.split('#'));
+    $(form).removeClass('mark');
+    $(form).removeClass('favorite');
+    $(form).addClass('unmark');
+    $(form).addClass('favorite');
+    $(form).click(unmark);
+
+}
+
+function unmark(event){
+    event.preventDefault();
+    var form = event.target;
+    $(form).removeClass('unmark');
+    $(form).removeClass('favorite');
+    $(form).addClass('mark');
+    $(form).addClass('favorite');
+    $(form).click(mark);
+
+}
+
+$('.mark').click(function(event){
+    event.preventDefault();
+    var form = event.target;
+    $(form).removeClass('mark');
+    $(form).removeClass('favorite');
+    $(form).addClass('unmark');
+    $(form).addClass('favorite');
+    $(form).click(unmark);
+
 });
 
-$('.unmark.favorite').click(function(event){
+$('.unmark').click(function(event){
     event.preventDefault();
     var form = event.target;
-    alert(form.split('#'));
+    $(form).removeClass('unmark');
+    $(form).removeClass('favorite');
+    $(form).addClass('mark');
+    $(form).addClass('favorite');
+    $(form).click(mark);
+
 });
